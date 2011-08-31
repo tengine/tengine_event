5ae2e7490582fbcf6c282fed0832ec433daf342f
t-akima <akima@cloud-dev-6.local>
2011-08-31 22:06:23 +0900

    [tengine_eventを作成する] Tengine::Event.newに属性値を指定できるようにしました

--- lib/tengine/event.rb
+++ lib/tengine/event.rb
@@ -5,12 +5,18 @@ require 'active_support/core_ext/object/blank'
 require 'uuid'
 
 class Tengine::Event
-  def initialize
+  def initialize(attrs = nil)
+    if attrs
+      raise ArgumentError, "attrs must be a Hash but #{attrs.inspect}" unless attrs.is_a?(Hash)
+      attrs.each do |key, value|
+        send("#{key}=", value)
+      end
+    end
     # uuidtools と uuid のどちらが良いかは以下のサイトを参照して uuid を使うようにしました。
     # http://d.hatena.ne.jp/kiwamu/20090205/1233826235
     @@uuid_gen ||= ::UUID.new
-    @key = @@uuid_gen.generate # Stringを返す
-    @properties = {}
+    @key ||= @@uuid_gen.generate # Stringを返す
+    @properties ||= {}
   end
 
   # @attribute
@@ -19,11 +25,13 @@ class Tengine::Event
 
   # @attribute
   # イベント種別名。
-  attr_accessor :event_type_name
+  attr_reader :event_type_name
+  def event_type_name=(v); @event_type_name = v.nil? ? nil : v.to_s; end
 
   # @attribute
   # イベントの発生源の識別名。
-  attr_accessor :source_name
+  attr_reader :source_name
+  def source_name=(v); @source_name = v.nil? ? nil : v.to_s; end
 
   # @attribute
   # イベントの発生日時。
@@ -31,5 +39,9 @@ class Tengine::Event
 
   # @attribute
   # プロパティ。他の属性だけでは表現できない諸属性を格納するHashです。
-  attr_accessor :properties
+  attr_reader :properties
+  def properties=(hash)
+    @properties = (hash || {}).inject({}){|d, (k,v)| d[k.to_s] = v; d}
+  end
+
 end
--- spec/tengine/event_spec.rb
+++ spec/tengine/event_spec.rb
@@ -11,4 +11,21 @@ describe "Tengine::Event" do
     its(:properties){ should be_a(Hash) }
     its(:properties){ should be_empty }
   end
+
+  describe :new_object_with_attrs do
+    subject{ Tengine::Event.new(
+        :event_type_name => :foo,
+        :key => "hoge",
+        :source_name => "server1",
+        :occurred_at => Time.utc(2011,8,11,12,0),
+        :properties => {:bar => "ABC", :baz => 999}
+        )}
+    it{ subject.should be_a(Tengine::Event) }
+    its(:key){ should == "hoge" }
+    its(:event_type_name){ should == "foo" }
+    its(:source_name){ should == "server1" }
+    its(:occurred_at){ should == Time.utc(2011,8,11,12,0) }
+    its(:properties){ should == {'bar' => "ABC", 'baz' => 999}}
+  end
+
 end
