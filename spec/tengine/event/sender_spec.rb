e5488af0decb1f666926f938c19846c1666cc806
shyouhei <shyouhei@ruby-lang.org>
2011-10-28 14:36:39 +0900

    [Tengine::Event::Sender#fireで使用している@retry_countが複数のfire間で混ざっている] 修正しました

--- spec/tengine/event/sender_spec.rb
+++ spec/tengine/event/sender_spec.rb
@@ -59,14 +59,14 @@ describe "Tengine::Event::Sender" do
         occurred_at = Time.now
         expected_event = Tengine::Event.new(:event_type_name => :foo, :key => "uniq_key", :occurred_at => occurred_at)
         @mock_exchange.should_receive(:publish).with(expected_event.to_json, :persistent => true)
-        EM.should_receive(:add_timer).with(1)
+        EM.stub(:add_timer).with(1)
         @sender.fire(:foo, :key => "uniq_key", :occurred_at => occurred_at)
       end
 
       it "Tengine::Eventオブジェクトを直接指定することも可能" do
         expected_event = Tengine::Event.new(:event_type_name => :foo, :key => "uniq_key")
         @mock_exchange.should_receive(:publish).with(expected_event.to_json, :persistent => true)
-        EM.should_receive(:add_timer).with(1)
+        EM.stub(:add_timer).with(1)
         @sender.fire(expected_event)
       end
 
@@ -75,7 +75,7 @@ describe "Tengine::Event::Sender" do
           expected_event = Tengine::Event.new(:event_type_name => :foo, :key => "uniq_key")
           @mock_exchange.should_receive(:publish).with(expected_event.to_json, :persistent => true).and_yield
           @mock_connection.should_receive(:disconnect).and_yield
-          EM.should_receive(:add_timer).with(1)
+          EM.stub(:add_timer).with(1)
           EM.should_receive(:stop)
           block_called = false
           @sender.fire(expected_event){ block_called = true }
@@ -86,7 +86,7 @@ describe "Tengine::Event::Sender" do
           expected_event = Tengine::Event.new(:event_type_name => :foo, :key => "uniq_key")
           @mock_exchange.should_receive(:publish).with(expected_event.to_json, :persistent => true).and_yield
           @mock_connection.should_not_receive(:disconnect)
-          EM.should_receive(:add_timer).with(1)
+          EM.stub(:add_timer).with(1)
           EM.should_not_receive(:stop)
           block_called = false
           @sender.default_keep_connection = true
@@ -102,14 +102,16 @@ describe "Tengine::Event::Sender" do
         # テスト実行時に1秒×30回、掛かるのは困るので、default値を変更しています。
         @sender = Tengine::Event::Sender.new(:exchange => {:name => "exchange1"}, :sender => {:retry_interval => 0})
       end
-
       it "メッセージ送信ができなくてpublishに渡したブロックが呼び出されず、インターバルが過ぎて、EM.add_timeに渡したブロックが呼び出された場合" do
         expected_event = Tengine::Event.new(:event_type_name => :foo, :key => "uniq_key")
         @mock_exchange.should_receive(:publish).with(expected_event.to_json, :persistent => true).exactly(31).times
         EM.should_receive(:add_timer).with(0).exactly(31).times.and_yield
+        EM.stub(:add_timer).with(0, an_instance_of(Proc)) {|k, v| v.call }
         block_called = false
         expect{
-          @sender.fire(expected_event){ block_called = true }
+          EM.run {
+            @sender.fire(expected_event){ block_called = true }
+          }
         }.to raise_error(Tengine::Event::Sender::RetryError)
         block_called.should == false
       end
@@ -118,11 +120,13 @@ describe "Tengine::Event::Sender" do
         expected_event = Tengine::Event.new(:event_type_name => :foo, :key => "uniq_key")
         lambda {
           begin
-            # 正規のfireとリトライのfireなので、リトライ回数+1
-            @mock_exchange.should_receive(:publish).with(expected_event.to_json, {:persistent=>true}).exactly(31).times.and_raise('error')
-            @sender.fire(expected_event)
+            EM.run {
+              # 正規のfireとリトライのfireなので、リトライ回数+1
+              @mock_exchange.should_receive(:publish).with(expected_event.to_json, {:persistent=>true}).exactly(31).times.and_raise('error')
+              @sender.fire(expected_event)
+            }
           rescue Tengine::Event::Sender::RetryError => e
-            e.message.should =~ /^event #<Tengine::Event:[^\]]+> has be tried to send 30 times. The last source exception was #<RuntimeError: error>$/
+            e.message.should =~ /^failed 30 time\(s\) to send event #<Tengine::Event:[^\]]+>.  The last source exception was #<RuntimeError: error>$/
             e.to_s.should == e.message
             raise e
           end
@@ -132,21 +136,91 @@ describe "Tengine::Event::Sender" do
       it "エラーが発生してもオプションで指定したリトライ回数分のリトライが行われる" do
         expected_event = Tengine::Event.new(:event_type_name => :foo, :key => "uniq_key")
         lambda {
-          # 正規のfireとリトライのfireなので、リトライ回数+1
-          @mock_exchange.should_receive(:publish).with(expected_event.to_json, {:persistent=>true}).exactly(2).times.and_raise('error')
-          @sender.fire(expected_event, :retry_count => 1)
+          EM.run {
+            # 正規のfireとリトライのfireなので、リトライ回数+1
+            @mock_exchange.should_receive(:publish).with(expected_event.to_json, {:persistent=>true}).exactly(2).times.and_raise('error')
+            @sender.fire(expected_event, :retry_count => 1)
+          }
         }.should raise_error(Tengine::Event::Sender::RetryError)
       end
 
       it "エラーが発生してもオプションで指定したリトライ間隔でリトライが行われる" do
         expected_event = Tengine::Event.new(:event_type_name => :foo, :key => "uniq_key")
         lambda {
-          # 正規のfireとリトライのfireなので、リトライ回数+1
-          @mock_exchange.should_receive(:publish).with(expected_event.to_json, {:persistent=>true}).exactly(4).times.and_raise('error')
-          @sender.fire(expected_event, :retry_count => 3, :retry_interval => 0)
+          EM.run {
+            # 正規のfireとリトライのfireなので、リトライ回数+1
+            @mock_exchange.should_receive(:publish).with(expected_event.to_json, {:persistent=>true}).exactly(4).times.and_raise('error')
+            @sender.fire(expected_event, :retry_count => 3, :retry_interval => 0)
+          }
         }.should raise_error(Tengine::Event::Sender::RetryError)
+      end
+
+      it "ちょうどretry_count回めのリトライして成功の場合は例外にならない" do
+        expected_event = mock(Tengine::Event.new(:event_type_name => :foo, :key => "uniq_key"))
+        lambda {
+          x = false
+          expected_event.stub(:to_json) do
+            if (x = !x)
+              raise "foo"
+            else
+              next "foo"
+            end
+          end
+          EM.run {
+            @sender.fire(expected_event, :retry_count => 1)
+          }
+        }.should_not raise_error(Tengine::Event::Sender::RetryError)
+      end
+    end
+
+    context "入り乱れたfireにおけるretryの回数" do
+      subject { Tengine::Event::Sender.new(:exchange => {:name => "exchange1"}, :sender => {:retry_interval => 0}) }
+      it "https://www.pivotaltracker.com/story/show/20236589" do
+        n1 = 0
+        n2 = 0
+        ev1 = mock(Tengine::Event.new(:event_type_name => :foo, :key => "uniq_key"))
+        ev2 = mock(Tengine::Event.new(:event_type_name => :foo, :key => "another_uniq_key"))
+        ev1.stub(:to_json) { n1 += 1; n1.to_s }
+        ev2.stub(:to_json) { n2 += 1; n2.to_s }
+        @mock_exchange.stub(:publish).with(an_instance_of(String), {:persistent=>true}).and_raise('error')
+        begin
+          EM.run do
+            EM.next_tick do
+              subject.fire(ev1, :keep_connection => true)
+              subject.fire(ev2, :keep_connection => true)
+            end
+          end
+        rescue Tengine::Event::Sender::RetryError => e
+          case e.event
+          when ev1
+            n1.should == 30
+            n2.should <= 29
+            n2.should >= 2
+          when ev2
+            n2.should == 30
+            n1.should <= 29
+            n1.should >= 2
+          end
+        end
+      end
+
+      it "無限にメモリを消費しない" do
+        n = 256 # 1024 # 4096
+        @mock_exchange.stub(:publish).with(an_instance_of(String), {:persistent=>true}).and_raise('error')
+        begin
+          EM.run do
+            n.times do
+              EM.next_tick do
+                ev = Tengine::Event.new(:event_type_name => :foo, :key => "uniq_key")
+                subject.fire(ev, :keep_connection => true, :retry_cont => 3)
+              end
+            end
+          end
+        rescue Tengine::Event::Sender::RetryError => e
+          GC.start
+          subject.__instrument.should < n
+        end
       end
     end
   end
-
 end
