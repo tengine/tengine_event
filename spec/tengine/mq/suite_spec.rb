67b7544c0df77268cc4624ab70eddc0075216fa6
shyouhei <shyouhei@ruby-lang.org>
2011-11-04 18:01:25 +0900

    Enumerable#each_next_tick のspecをtengine_coreから移動しました。

--- spec/tengine/mq/suite_spec.rb
+++ spec/tengine/mq/suite_spec.rb
@@ -1,8 +1,35 @@
 # -*- coding: utf-8 -*-
-p# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
+require_relative '../../../lib/tengine/mq/suite'
 require 'amqp'
+
+describe Enumerable do
+  describe "#each_next_tick" do
+    it "eachと同じ順にiterateする" do
+      str = ""
+      EM.run do
+        [1, 2, 3, 4].each_next_tick do |i|
+          str << i.to_s
+        end
+        EM.add_timer 0.1 do EM.stop end
+      end
+      str.should == "1234"
+    end
+
+    it "next_tickでやる" do
+      str = ""
+      EM.run do
+        [1, 2, 3, 4].each_next_tick do |i|
+          str << i.to_s
+        end
+        str.should == ""
+        EM.add_timer 0.1 do EM.stop end
+      end
+    end
+  end
+end
 
 describe "Tengine::Mq::Suite" do
 
